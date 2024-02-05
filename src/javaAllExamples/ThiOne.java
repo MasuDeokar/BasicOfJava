8e48226a8b6eae839a520d96f031118bb1a02daa
MasuDeokar <mayureshdeokar@gmail.com>
2024-02-05 23:25:41 +0530

    first commit

---
+++ src/javaAllExamples/ThiOne.java
@@ -0,0 +1,17 @@
+package javaAllExamples;
+class ThiOne {
+		int i;
+		void display(int i)
+		{
+			 this.i =i;
+		}
+		void show()
+		{
+			System.out.println(i);
+		}
+			public static void main(String[] args) 
+		{		
+		ThiOne call=new ThiOne();
+  		call.show();
+		}
+	}
